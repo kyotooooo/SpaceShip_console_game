13034ca9715dd90afd9444ad2500c02f40ebd6b4
kyotooooo <kotsyram@gmail.com>
2024-06-04 14:28:02 +0300

    added randomNumber func

---
+++ randomNumber.cpp
@@ -0,0 +1,7 @@
+#include <cstdlib>
+
+int getRandomNumber(int min, int max)
+{
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
